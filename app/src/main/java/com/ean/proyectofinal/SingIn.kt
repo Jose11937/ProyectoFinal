3b0dc8ca9fd4c4754d759a023252908518d35d59
Jose Vicente Suarez Grisales <jsuarez78942@universidadean.edu.co>
2022-05-22 12:26:08 -0500

    Initial commit

---
+++ app/src/main/java/com/ean/proyectofinal/SingIn.kt
@@ -0,0 +1,11 @@
+package com.ean.proyectofinal
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SingIn : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_in)
+    }
+}
