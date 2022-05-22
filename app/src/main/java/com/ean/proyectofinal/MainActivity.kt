3608a47eb9172481faea1833f67183b10fb54d73
Jose Vicente Suarez Grisales <jsuarez78942@universidadean.edu.co>
2022-05-22 12:49:18 -0500

    Origin

--- app/src/main/java/com/ean/proyectofinal/MainActivity.kt
+++ app/src/main/java/com/ean/proyectofinal/MainActivity.kt
@@ -2,10 +2,20 @@ package com.ean.proyectofinal
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.ean.proyectofinal.databinding.ActivityMainBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        auth = Firebase.auth
     }
 }
