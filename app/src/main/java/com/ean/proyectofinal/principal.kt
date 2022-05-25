2e4a5795ab3ff0bc1a3a4e738212231f82f5edd8
Jose Vicente Suarez Grisales <jsuarez78942@universidadean.edu.co>
2022-05-25 12:23:15 -0500

    Origin

--- app/src/main/java/com/ean/proyectofinal/principal.kt
+++ app/src/main/java/com/ean/proyectofinal/principal.kt
@@ -1,14 +1,28 @@
 package com.ean.proyectofinal
 
-import android.os.Bundle
-import android.os.PersistableBundle
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageButton
 
+class principal : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_principal)
 
-class principal : AppCompatActivity(){
+        val userregresar = findViewById<ImageButton>(R.id.imageButton5)
 
+        userregresar.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+        val userb= findViewById<ImageButton>(R.id.imageButton6)
+
+        userb.setOnClickListener {
+            val intent = Intent(this, principal::class.java)
+            startActivity(intent)
+        }
+    }
 }
-
-
-
-
