200e3785068adea8a91b9ab7b30befbf14754fe6
Isabel-Zamudio <73199137+Isabel-Zamudio@users.noreply.github.com>
2022-05-26 16:34:24 -0500

    Comida 3

---
+++ app/src/main/java/com/ean/proyectofinal/MostrarBurrrito.kt
@@ -0,0 +1,27 @@
+package com.ean.proyectofinal
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageButton
+
+class MostrarBurrrito : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_mostrar_burrito)
+
+        val userregresar = findViewById<ImageButton>(R.id.imageButton5)
+
+        userregresar.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+        val userb= findViewById<ImageButton>(R.id.imageButton6)
+
+        userb.setOnClickListener {
+            val intent = Intent(this, principal::class.java)
+            startActivity(intent)
+        }
+    }
+}
