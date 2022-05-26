5e4b21c513d882f7781dc42770885f3010e5d122
Isabel-Zamudio <73199137+Isabel-Zamudio@users.noreply.github.com>
2022-05-26 16:39:34 -0500

    Comida 3

--- app/src/main/java/com/ean/proyectofinal/MostrarBurger.kt
+++ app/src/main/java/com/ean/proyectofinal/MostrarBurger.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.widget.ImageButton
 
 class MostrarBurger : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_mostrar_burger)
